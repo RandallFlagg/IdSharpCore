291dce4a6ba377de7cac78f1085adabf60d9930d
Randall Flagg <O@G>
2025-06-29 01:13:07 +0300

    Removed windows support

--- IdSharp.Tagging/Mpeg4Tag/Mpeg4Tag.cs
+++ IdSharp.Tagging/Mpeg4Tag/Mpeg4Tag.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.IO;
 using System.Text;
+
 using IdSharp.Common.Utils;
 using IdSharp.Tagging.ID3v1;
 
@@ -425,7 +423,10 @@ namespace IdSharp.Tagging.Mpeg4
 
         private void ParseTag(string key, byte[] data, int size)
         {
-            if (_isWriting) return;
+            if (_isWriting)
+            {
+                return;
+            }
 
             if (string.Compare(key, "©nam", true) == 0)
             {
