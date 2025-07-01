bc4b474b5827c3d4b32edf41e6d9a1f1ae47922a
Randall Flagg <O@G>
2025-07-01 19:06:49 +0300

    Adding back nuget support

--- IdSharp.Tagging/nugetPush.cmd
+++ IdSharp.Tagging/nugetPush.cmd
@@ -4,6 +4,7 @@ set TAGGING_VERSION=1.0.0-rc4
 set TAGGING_NUPKG_FILE=%CD%\bin\release\IdSharp.Tagging.%TAGGING_VERSION%.nupkg
 
 dotnet pack "%CD%\IdSharp.Tagging-core.csproj" -c release
+::Release or release?
 mkdir "%DEPLOY_DIR%"
 copy "%TAGGING_NUPKG_FILE%" "%DEPLOY_DIR%"
 dotnet nuget push "%TAGGING_NUPKG_FILE%" -k %1 -s https://api.nuget.org/v3/index.json
