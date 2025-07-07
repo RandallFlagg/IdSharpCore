7b3777491f55500dc52fc34aefa88fbe18852e85
Randall Flagg <O@G>
2025-07-07 12:35:03 +0300

    dfdf

--- Tests/IdSharp.AudioInfo.Tests/Mpeg/PresetGuesserTests.cs
+++ Tests/IdSharp.AudioInfo.Tests/Mpeg/PresetGuesserTests.cs
@@ -15,67 +15,71 @@ public class PresetGuesserTests
     {
         var expected = new List<PresetGuessRow>
         {
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, tv1: 255, tv2: 58, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 205, result: LamePreset.Insane),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 255, tv2: 58, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 206, result: LamePreset.Insane),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 255, tv2: 57, tv3: 1, tv4: 1, tv5: 3, tv6: 4, tv7: 205, result: LamePreset.Insane),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.Extreme),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3902_391, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.Extreme),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 3, tv4: 1, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.Extreme),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.FastExtreme),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.FastExtreme),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.Standard),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 3, tv4: 1, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.Standard),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, vg3: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.FastStandard),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 68, tv3: 3, tv4: 2, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.Medium),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 68, tv3: 4, tv4: 2, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.FastMedium),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 88, tv3: 4, tv4: 1, tv5: 3, tv6: 3, tv7: 195, result: LamePreset.R3mix),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 88, tv3: 4, tv4: 1, tv5: 3, tv6: 3, tv7: 196, result: LamePreset.R3mix),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 255, tv2: 99, tv3: 1, tv4: 1, tv5: 1, tv6: 2, tv7: 0, result: LamePreset.Studio),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 255, tv2: 58, tv3: 2, tv4: 1, tv5: 3, tv6: 2, tv7: 206, result: LamePreset.Studio),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, tv1: 255, tv2: 58, tv3: 2, tv4: 1, tv5: 3, tv6: 2, tv7: 205, result: LamePreset.Studio),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 255, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 205, result: LamePreset.Studio),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 192, tv2: 88, tv3: 1, tv4: 1, tv5: 1, tv6: 2, tv7: 0, result: LamePreset.CD),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 192, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.CD),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, tv1: 192, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.CD),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 192, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 195, result: LamePreset.CD),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 160, tv2: 78, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Hifi),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 160, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Hifi),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 160, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.Hifi),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 128, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Tape),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 128, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 150, result: LamePreset.Radio),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 112, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 150, result: LamePreset.FM),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 112, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 160, result: LamePreset.TapeRadioFM),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 112, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 160, result: LamePreset.TapeRadioFM),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 56, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 100, result: LamePreset.Voice),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 56, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 150, result: LamePreset.Voice),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg390_3901_392, tv1: 40, tv2: 65, tv3: 1, tv4: 1, tv5: 0, tv6: 2, tv7: 75, result: LamePreset.MWUS),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3902_391, tv1: 40, tv2: 65, tv3: 1, tv4: 1, tv5: 0, tv6: 2, tv7: 76, result: LamePreset.MWUS),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 40, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 70, result: LamePreset.MWUS),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 40, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 105, result: LamePreset.MWUS),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 24, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 40, result: LamePreset.MWEU),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, tv1: 24, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 39, result: LamePreset.MWEU),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 24, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 59, result: LamePreset.MWEU),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg3931_3903up, tv1: 16, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 38, result: LamePreset.Phone),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg393, tv1: 16, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 37, result: LamePreset.Phone),
-            new PresetGuessRow(vg1: LameVersionGroup.lvg394up, tv1: 16, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 56, result: LamePreset.Phone)
+            new(vg1: LameVersionGroup.lvg390_3901_392, tv1: 255, tv2: 58, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 205, result: LamePreset.Insane),
+            new(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 255, tv2: 58, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 206, result: LamePreset.Insane),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 255, tv2: 57, tv3: 1, tv4: 1, tv5: 3, tv6: 4, tv7: 205, result: LamePreset.Insane),
+            new(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.Extreme),
+            new(vg1: LameVersionGroup.lvg3902_391, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.Extreme),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 3, tv4: 1, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.Extreme),
+            new(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.FastExtreme),
+            new(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.FastExtreme),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 0, tv2: 78, tv3: 3, tv4: 2, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.Standard),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 3, tv4: 1, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.Standard),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, vg3: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 78, tv3: 4, tv4: 2, tv5: 3, tv6: 4, tv7: 190, result: LamePreset.FastStandard),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 68, tv3: 3, tv4: 2, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.Medium),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 68, tv3: 4, tv4: 2, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.FastMedium),
+            new(vg1: LameVersionGroup.lvg390_3901_392, tv1: 0, tv2: 88, tv3: 4, tv4: 1, tv5: 3, tv6: 3, tv7: 195, result: LamePreset.R3mix),
+            new(vg1: LameVersionGroup.lvg3902_391, vg2: LameVersionGroup.lvg3931_3903up, tv1: 0, tv2: 88, tv3: 4, tv4: 1, tv5: 3, tv6: 3, tv7: 196, result: LamePreset.R3mix),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 255, tv2: 99, tv3: 1, tv4: 1, tv5: 1, tv6: 2, tv7: 0, result: LamePreset.Studio),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 255, tv2: 58, tv3: 2, tv4: 1, tv5: 3, tv6: 2, tv7: 206, result: LamePreset.Studio),
+            new(vg1: LameVersionGroup.lvg393, tv1: 255, tv2: 58, tv3: 2, tv4: 1, tv5: 3, tv6: 2, tv7: 205, result: LamePreset.Studio),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 255, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 205, result: LamePreset.Studio),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 192, tv2: 88, tv3: 1, tv4: 1, tv5: 1, tv6: 2, tv7: 0, result: LamePreset.CD),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 192, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 196, result: LamePreset.CD),
+            new(vg1: LameVersionGroup.lvg393, tv1: 192, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 195, result: LamePreset.CD),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 192, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 195, result: LamePreset.CD),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 160, tv2: 78, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Hifi),
+            new(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 160, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Hifi),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 160, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 180, result: LamePreset.Hifi),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 128, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 180, result: LamePreset.Tape),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 128, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 150, result: LamePreset.Radio),
+            new(vg1: LameVersionGroup.lvg390_3901_392, vg2: LameVersionGroup.lvg3902_391, tv1: 112, tv2: 67, tv3: 1, tv4: 1, tv5: 3, tv6: 2, tv7: 150, result: LamePreset.FM),
+            new(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 112, tv2: 58, tv3: 2, tv4: 2, tv5: 3, tv6: 2, tv7: 160, result: LamePreset.TapeRadioFM),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 112, tv2: 57, tv3: 2, tv4: 1, tv5: 3, tv6: 4, tv7: 160, result: LamePreset.TapeRadioFM),
+            new(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 56, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 100, result: LamePreset.Voice),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 56, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 150, result: LamePreset.Voice),
+            new(vg1: LameVersionGroup.lvg390_3901_392, tv1: 40, tv2: 65, tv3: 1, tv4: 1, tv5: 0, tv6: 2, tv7: 75, result: LamePreset.MWUS),
+            new(vg1: LameVersionGroup.lvg3902_391, tv1: 40, tv2: 65, tv3: 1, tv4: 1, tv5: 0, tv6: 2, tv7: 76, result: LamePreset.MWUS),
+            new(vg1: LameVersionGroup.lvg393, vg2: LameVersionGroup.lvg3931_3903up, tv1: 40, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 70, result: LamePreset.MWUS),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 40, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 105, result: LamePreset.MWUS),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 24, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 40, result: LamePreset.MWEU),
+            new(vg1: LameVersionGroup.lvg393, tv1: 24, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 39, result: LamePreset.MWEU),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 24, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 59, result: LamePreset.MWEU),
+            new(vg1: LameVersionGroup.lvg3931_3903up, tv1: 16, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 38, result: LamePreset.Phone),
+            new(vg1: LameVersionGroup.lvg393, tv1: 16, tv2: 58, tv3: 2, tv4: 2, tv5: 0, tv6: 2, tv7: 37, result: LamePreset.Phone),
+            new(vg1: LameVersionGroup.lvg394up, tv1: 16, tv2: 57, tv3: 2, tv4: 1, tv5: 0, tv6: 4, tv7: 56, result: LamePreset.Phone)
         };
 
         //IList<PresetGuessRow> actual = PresetGuesser();
         List<PresetGuessRow> actual = null; // Replace with actual call to PresetGuesser() method
         Assert.That(actual.Count, Is.EqualTo(expected.Count), "Preset row count mismatch");
 
-        for (int i = 0; i < expected.Count; i++)
+        for (var i = 0; i < expected.Count; i++)
         {
             var a = actual[i];
             var e = expected[i];
 
             Assert.That(a.Res, Is.EqualTo(e.Res), $"Row {i} - Res mismatch");
 
-            for (int t = 0; t < 7; t++)
+            for (var t = 0; t < 7; t++)
+            {
                 Assert.That(a.TVs[t], Is.EqualTo(e.TVs[t]), $"Row {i} - TVs[{t}] mismatch");
+            }
 
-            for (int v = 0; v < 3; v++)
+            for (var v = 0; v < 3; v++)
+            {
                 Assert.That(a.VGs[v], Is.EqualTo(e.VGs[v]), $"Row {i} - VGs[{v}] mismatch");
+            }
         }
 
 
@@ -103,8 +107,6 @@ public class PresetGuesserTests
             Assert.That(act.VGs[1], Is.EqualTo(vg1), msg + "VG2 mismatch");
             var vg2 = exp.VGs[2] != LameVersionGroup.None ? exp.VGs[2] : LameVersionGroup.None;
             Assert.That(act.VGs[2], Is.EqualTo(vg2), msg + "VG3 mismatch");
-
-
         }
     }
 }
